df058c57de6134d9eaf9f78722a87fef80883fbf
James Byess <james.byess@gmail.com>
2019-04-28 22:40:14 -0400

    update getpeople

--- db/getPeople.js
+++ db/getPeople.js
@@ -7,14 +7,17 @@ let peopleCount = 20
 
 let ids = []
 
+// build array of random ids
 for(let i = 0; i < peopleCount; i++) {
 	let rand = Math.floor(Math.random() * 10000)
 	ids.push(rand)
 }
 
+// build 20 urls to make calls to, and return array of promises with those urls
 let calls = ids.map(id => `${baseURL}${id}`)
 .map(url => axios.get(url))
 
+// execute all promises, writing to disk if successful
 Promise.all(calls)
 .then(success => {
 	let collectedData = success.map(res => res.data)
@@ -29,7 +32,7 @@ Promise.all(calls)
 	}) 
 })
 .catch(err => {
-	console.error(err.data)
+	console.error(err)
 	console.error('there was probably an issue with the rate limit, try again in 10 seconds or check the error messages above.')
 })
 
